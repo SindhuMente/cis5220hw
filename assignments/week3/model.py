df8c818a72916e92f2ed6dc68b2c8d4137b68c82
Sindhura Mente <sindhuramente@gmail.com>
2023-02-06 21:28:32 -0500

    hw2

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -2,14 +2,18 @@ import torch
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
+    """
+    Our MLP class
+    """
+
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
         num_classes: int,
         hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
+        activation: Callable = torch.nn.ReLU(),
         initializer: Callable = torch.nn.init.ones_,
     ) -> None:
         """
@@ -22,9 +26,20 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        self.layer = torch.nn.Linear(input_size, hidden_size)
+        self.hidden_layers = torch.nn.ModuleList()
+        initializer(self.layer.weight)
+        self.activation = activation
+        # input_dim = 32
+        # self. hidden_units = [16, 8, 4]
+        # A fully-connected network (FCN) with len(hidden_units) hidden layers
+        for _ in range(1, hidden_count + 1):
+            self.hidden_layers += [torch.nn.Linear(hidden_size, hidden_size)]
 
-    def forward(self, x):
+        self.out = torch.nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x:torch.Tensor) -> None:
         """
         Forward pass of the network.
 
@@ -34,4 +49,11 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        x = self.layer(x)
+        act = self.activation
+        x1 = act(x)
+        for layer in self.hidden_layers:
+            x = layer(x1)
+            x1 = act(x)
+        # x_out = self.layer(x1)
+        return self.out(x1)
