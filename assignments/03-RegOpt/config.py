707630f8eadca8d28d1be9a7395693033280ec9b
Sindhura Mente <sindhuramente@gmail.com>
2023-02-18 16:59:11 -0500

    hw3

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -15,7 +15,7 @@ class CONFIG:
     """config 1"""
     batch_size = 64
     num_epochs = 6
-    initial_learning_rate = 0.005
+    initial_learning_rate = 0.001
     initial_weight_decay = initial_learning_rate / num_epochs
 
     # You can pass arguments to the learning rate scheduler
